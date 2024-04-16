defb0bdc26e0a2536c00d6afc67cf9611426f349
fanyonglong <fanyonglong@qq.com>
2024-04-16 11:34:37 +0800

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-
+import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve:{
+
+  },
   plugins: [svelte()],
 })
